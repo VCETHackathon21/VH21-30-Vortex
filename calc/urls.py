28bbb06775ca3906a25db86dc036d46ad54b3550
bighac <2018.ajay.gupta@ves.ac.in>
2021-03-19 22:14:09 +0530

    login and register page is added

--- calc/urls.py
+++ calc/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('' , views.home , name='home'),
     path('addpost', views.addpost , name='addpost'),
-    path('add' , views.add , name='add')
+    path('add' , views.add , name='add'),
+    path('login', views.login,  name='login'),
+    path('signup' , views.signup , name='signup'),
+    path('signuppost', views.signuppost, name="signuppost"),
+    path('loginpost', views.loginpost , name="loginpost")
 ]
--- calc/views.py
+++ calc/views.py
@@ -2,11 +2,58 @@ from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Destination
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+
 # Create your views here.
 def home(request):
     dist = Destination.objects.all()
     return render(request , 'base.html' , {'data' : dist })
 
+def login(request):
+    return render(request , 'login.html')
+
+def signup(request):
+    return render(request , 'signup.html')
+
+
+def loginpost(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    # Check and login 
+    return redirect("/")
+
+
+def signuppost(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    cpassword = request.POST['cpassword'] 
+    email = request.POST['email']
+    firstname = username
+    lastname = username
+
+    # Craete new user here..
+
+    if password == cpassword:
+        if User.objects.filter(username=username).exists():
+            # Redirect them to signup page
+            messages.info(request , "Username Already Exist")
+            return redirect("/signup")
+        elif User.objects.filter(email=email).exists():
+            # Redirect them to signup page
+            messages.info(request , "Email Is Already Taken")
+            return redirect("/signup")
+        else:
+            user = User.objects.create_user(first_name=firstname , last_name=lastname, email=email, password=password, username=username)
+            user.save()
+            print("User created" , user)
+            return redirect("/")
+       
+    else:
+        messages.info(request , "Passwords does not match")
+        return redirect("/signup")
+     
+    
 
 def addpost(request):
     return render(request , 'addpost.html')
@@ -22,4 +69,3 @@ def add(request):
     data1.save()
 
     return redirect("/")
-    # return render(request , 'result.html' ,  { 'imgurl' : imgurl , 'title' : title ,'username' : username , 'desc' : desc })
